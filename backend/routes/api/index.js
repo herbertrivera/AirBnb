99385aa73bee3cd5bb0fbfb21ec89ce1a9e31584
Herbert Rivera <herbertrivera@yahoo.com>
2023-04-19 18:59:03 -0400

    updated api

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -5,5 +5,16 @@ router.post('/test', function(req, res) {
     res.json({ requestBody: req.body });
   });
 
+  const { setTokenCookie } = require('../../utils/auth.js');
+  const { User } = require('../../db/models');
+  router.get('/set-token-cookie', async (_req, res) => {
+    const user = await User.findOne({
+      where: {
+        username: 'Demo-lition'
+      }
+    });
+    setTokenCookie(res, user);
+    return res.json({ user: user });
+  });
 
 module.exports = router;
